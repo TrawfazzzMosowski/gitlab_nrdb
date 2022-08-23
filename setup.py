b75c9ab54d7a2d1af722ae0b7a848b1c1c2703da
mosowski <mosowski@ptd.net>
2022-08-22 21:49:27 -0400

    added setup.py for install

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='gitlab_nrdb',
+    version='',
+    packages=['nr_nrdb', 'gitlab_parse'],
+    url='',
+    license='',
+    author='mosowski',
+    author_email='',
+    description=''
+)
